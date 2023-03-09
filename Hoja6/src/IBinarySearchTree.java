bbf17d8fe2dbcaaff39b2458bb10546be3633686
jorgelopez <lop221038@uvg.edu.gt>
2023-03-08 19:03:30 -0600

    Se finalizo la interfaz IBinarySearchTree y la clase BinarySearchTree.java

--- Hoja6/src/IBinarySearchTree.java
+++ Hoja6/src/IBinarySearchTree.java
@@ -1,2 +1,31 @@
-public interface IBinarySearchTree {
+/**
+ *
+ */
+
+import java.util.ArrayList;
+
+/**
+ * @author Jorge Lopez
+ *
+ */
+public interface IBinarySearchTree<K, V> {
+
+    void insert(K id, V value);
+
+    V delete(K id);
+
+    V find(K id);
+
+    int count();
+
+    boolean isEmpty();
+
+    ArrayList<V> getElements();
+
+    void inOrder(ITreeTraversal<V> traversal);
+
+    void preOrder(ITreeTraversal<V> traversal);
+
+    void postOrder(ITreeTraversal<V> traversal);
+
 }

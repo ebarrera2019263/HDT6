4ac3dc07d2acc211d2353da57eb58740c7f3d415
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2023-03-08 18:10:23 -0600

    Se agrego la clase Archivo para leer el archivo de texto ListadoProducto.txt

---
+++ Hoja6/src/Archivo.java
@@ -0,0 +1,35 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Archivo {
+
+    ArrayList<String> array = new ArrayList<String>();
+
+    public boolean read() throws FileNotFoundException{
+        boolean read = false;
+        try{
+            read = true;
+            File file = new File("ListadoProducto.txt");
+            Scanner reader = new Scanner(file);
+            while(reader.hasNextLine()){
+                String dato = reader.nextLine();
+                String[] separada = dato.split("\\|");
+                for (int i=0; i<separada.length; i++)
+                    array.add(separada[i].trim());
+            }
+            reader.close();
+        }catch(FileNotFoundException e){
+            String s = "SortController.read:" + e.getMessage();
+            throw new FileNotFoundException(s);
+        }
+        return read;
+    }
+
+    public ArrayList<String> getArchivo(){
+        return this.array;
+
+    }
+
+}

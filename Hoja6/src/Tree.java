6c8d2722d99a5fb283966a6da9bf6b874d4352b9
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2023-03-08 18:49:39 -0600

    Se agrego la clase Tree

--- Hoja6/src/Tree.java
+++ Hoja6/src/Tree.java
@@ -1,2 +1,76 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+import java.util.*;
 public class Tree {
+    TreeMap<Integer, String> inventario = new TreeMap<Integer, String>();
+    TreeMap<Integer, String> categoria = new TreeMap<Integer, String>();
+    TreeMap<Integer, Integer> cantidad = new TreeMap<Integer, Integer>();
+    ArrayList<String> miArray = new ArrayList<String>();
+
+    public Tree(Archivo miArchivo){
+        miArray = miArchivo.getArchivo();
+        int x = 0;
+        for (int i=0; i<miArray.size(); i = i+2){
+            x++;
+            categoria.put(x,miArray.get(i));
+            cantidad.put(x,0);
+        }
+        x=0;
+        for(int j =1; j<miArray.size();j = j + 2){
+            x++;
+            inventario.put(x,miArray.get(j));
+        }
+    }
+    public void agregarProducto(String categoria, String nombreProducto, int cantProductos){
+        Integer getKeyFromValue = getSingleKeyFromValue(inventario, nombreProducto);
+        if(getKeyFromValue==null){
+            System.out.println("El producto al que desea agregar no existe");
+        }
+        else{
+            int cant = cantidad.get(getKeyFromValue);
+            cantidad.replace(getKeyFromValue,cant + cantProductos);
+        }
+    }
+
+    public void mostrarCategoria(String nombreProducto) {
+        Integer getKeyFromValue = getSingleKeyFromValue(inventario, nombreProducto);
+        System.out.println("Codigo:"+getKeyFromValue+ " Categoria: " + categoria.get(getKeyFromValue));
+    }
+
+    private static <K, V> K getSingleKeyFromValue(Map<K, V> map, V value) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            if (Objects.equals(value, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+    public void ProductosCaracteristicas() {
+        for (Integer key: inventario.keySet()){
+            System.out.println("Codigo:"+key+ " Categoria: " + categoria.get(key)+ " Nombre: " + inventario.get(key)+ " Cantidad: " + cantidad.get(key));
+        }
+    }
+
+    public void ProductosCaracteristicasOrdenadas() {
+        List<Entry<Integer, String>> list = new ArrayList<>(categoria.entrySet());
+        list.sort(Entry.comparingByValue());
+        for (int i =0; i<list.size();i++){
+            System.out.println("Codigo:"+list.get(i).getKey()+ " Categoria: " + categoria.get(list.get(i).getKey())+ " Nombre: " + inventario.get(list.get(i).getKey())+ " Cantidad: " + cantidad.get(list.get(i).getKey()));
+        }
+    }
+
+    public void productoCategoria() {
+        for (Integer key: inventario.keySet()){
+            System.out.println("Codigo:"+key+ " Categoria: " + categoria.get(key)+ " Nombre: " + inventario.get(key));
+        }
+    }
+
+    public void productoCategoriaOrdenada() {
+        List<Entry<Integer, String>> list = new ArrayList<>(categoria.entrySet());
+        list.sort(Entry.comparingByValue());
+        for (int i =0; i<list.size();i++){
+            System.out.println("Codigo:"+list.get(i).getKey()+ " Categoria: " + categoria.get(list.get(i).getKey())+ " Nombre: " + inventario.get(list.get(i).getKey()));
+        }
+    }
 }

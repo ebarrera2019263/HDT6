c7080dad5ac3613c3dd1ad8a3cec7b9dd42b3ed2
jorgelopez <lop221038@uvg.edu.gt>
2023-03-08 21:57:56 -0600

    class ControladoraTest

---
+++ Hoja6/src/ControladoraTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControladoraTest {
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}

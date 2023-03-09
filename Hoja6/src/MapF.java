eca1852ce0768d62b3e854001cc36d0f724a4bba
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2023-03-08 18:51:22 -0600

    Se agrego la clase hashmap

---
+++ Hoja6/src/MapF.java
@@ -0,0 +1,19 @@
+
+import java.util.HashMap;
+import java.util.Map;
+public class MapF {
+    private int tipo;
+    public void crearMap(int opcion) {};
+
+    public void agregarProducto(String categoria, String nombreProducto, int cantProductos) {};
+
+    public void mostrarCategoria(String nombreProducto) {};
+
+    public void ProductosCaracteristicas() {};
+
+    public void ProductosCaracteristicasOrdenadas() {};
+
+    public void productoCategoria() {};
+
+    public void productoCategoriaOrdenada() {};
+}
